1041c53d5ccae1bfafab59fac41e7a81f0061cd6
Administrator <admin@example.com>
2022-01-15 23:14:04 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python! XGenesisSS Inc"
+    return "Hello from Python! dmg blockchain solutions Inc"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
